72982ee550b2a9d5bd383b4ead406e37cb3c72e6
Jon Brick <jonbrick09@gmail.com>
2025-06-12 21:49:46 -0400

    Datepicker and linting done

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,20 @@ export const focusRing = [
   "focus:ring-offset-2",
   "dark:focus:ring-offset-gray-950",
 ];
+
+export const focusInput = [
+  "focus:outline-none",
+  "focus:ring-2",
+  "focus:ring-blue-500",
+  "focus:ring-offset-2",
+  "dark:focus:ring-offset-gray-950",
+];
+
+export const hasErrorInput = [
+  "border-red-500",
+  "focus:border-red-500",
+  "focus:ring-red-500",
+  "dark:border-red-500",
+  "dark:focus:border-red-500",
+  "dark:focus:ring-red-500",
+];
